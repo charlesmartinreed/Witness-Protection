62d0b73eb82e95f4debcf0a0bc08cc050f57b867
charlesmartinreed <charlesmartinreed@icloud.com>
2019-02-21 13:22:28 -0600

    Integrate boilerplate AVKit code for capturing and display video from the device input.

---
+++ Witness Protection/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  Witness Protection
+//
+//  Created by Charles Martin Reed on 2/21/19.
+//  Copyright © 2019 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+
+    
+}
--- Witness Protection/ViewController.swift
+++ Witness Protection/ViewController.swift
@@ -7,14 +7,174 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet weak var videoPreviewView: UIView?
+    
+    //MARK: AVCapture properties
+    var session: AVCaptureSession?
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    
+    var videoDataOutput: AVCaptureVideoDataOutput?
+    var videoDataOutputQueue: DispatchQueue?
+    
+    var captureDevice: AVCaptureDevice?
+    var captureDeviceResolution: CGSize = CGSize()
+    
+    //ui properties for drawing Vision results
+    var rootLayer: CALayer?
+    var detectionOverlayLayer: CALayer?
+    var detectedFaceRectangleShapeLayer: CAShapeLayer?
+    var detectedFaceLandmarksShapeLayer: CAShapeLayer?
+    
+    //vision request properties
+    private var detectionRequests: [VNDetectFaceRectanglesRequest]?
+    private var trackingRequests: [VNTrackingRequest]?
+    
+    lazy var sequenceRequestHandler = VNSequenceRequestHandler() //process request for each frame
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.session = self.setupAVCaptureSession()
+        
+        //self.prepareVisionRequest()
+        
+        self.session?.startRunning()
+        
     }
+    
+    //MARK:- Create a capture session for the video
+    fileprivate func setupAVCaptureSession() -> AVCaptureSession? {
+        let captureSession = AVCaptureSession()
 
+        do {
+            let inputDevice = try self.configureFrontCamera(for: captureSession)
+            self.configureVideoDataOutput(for: inputDevice.device, resolution: inputDevice.resolution, captureSession: captureSession)
+            self.designatePreviewLayer(for: captureSession)
+            return captureSession
+        } catch let execError as NSError {
+            self.presentError(execError)
+        } catch {
+            self.presentErrorAlert(message: "An unexpected failure has occured")
+        }
+        
+        //before we escape out, we need to dismantle our AVCapture
+        self.teardownAVCapture()
+        return nil
+    }
+    
+    
+    fileprivate func configureFrontCamera(for captureSession: AVCaptureSession) throws -> (device: AVCaptureDevice, resolution: CGSize) {
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .front)
+        
+        if let device = deviceDiscoverySession.devices.first {
+            if let deviceInput = try? AVCaptureDeviceInput(device: device) {
+                if captureSession.canAddInput(deviceInput) {
+                    captureSession.addInput(deviceInput)
+                }
+                
+                if let highestResolution = self.highestResolution420Format(for: device) {
+                    try device.lockForConfiguration()
+                    device.activeFormat = highestResolution.format
+                    device.unlockForConfiguration()
+                    
+                    return (device, highestResolution.resolution)
+                }
+            }
+        }
+        
+        throw NSError(domain: "ViewController", code: 1, userInfo: nil)
+    }
+    
+    //MARK:- Configuring the dispatchQueue
+    fileprivate func configureVideoDataOutput(for inputDevice: AVCaptureDevice, resolution: CGSize, captureSession: AVCaptureSession) {
+        let videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        
+        //serial dispatch queue, used for sample buffer delegate AND when a still image is captured. Serial queue because the frames must be delievered in order.
+        let videoDataOutputQueue = DispatchQueue(label: "com.charlesmartinreed-sample.Witness_Protection")
+        videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        
+        if captureSession.canAddOutput(videoDataOutput) {
+            captureSession.addOutput(videoDataOutput)
+        }
+        
+        videoDataOutput.connection(with: .video)?.isEnabled = true
+        
+        if let captureConnection = videoDataOutput.connection(with: .video) {
+            if captureConnection.isCameraIntrinsicMatrixDeliverySupported {
+                captureConnection.isCameraIntrinsicMatrixDeliveryEnabled = true
+            }
+        }
+    }
+    
+    //MARK:- Designate a preview layer
+    fileprivate func designatePreviewLayer(for captureSession: AVCaptureSession) {
+        let videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.previewLayer = videoPreviewLayer
+        
+        videoPreviewLayer.name = "CameraPreview"
+        videoPreviewLayer.backgroundColor = UIColor.black.cgColor
+        videoPreviewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        
+        if let previewRootLayer = self.videoPreviewView?.layer {
+            self.rootLayer = previewRootLayer
+            
+            previewRootLayer.masksToBounds = true
+            videoPreviewLayer.frame = previewRootLayer.bounds
+            previewRootLayer.addSublayer(videoPreviewLayer)
+        }
+    }
+    
+    //MARK:- Configure device resolution
+    fileprivate func highestResolution420Format(for device: AVCaptureDevice) -> (format: AVCaptureDevice.Format, resolution: CGSize)? {
+        var highestResolutionFormat: AVCaptureDevice.Format? = nil
+        var highestResolutionDimensions = CMVideoDimensions(width: 0, height: 0)
+        
+        for format in device.formats {
+            let deviceFormat = format as AVCaptureDevice.Format
+            
+            let deviceFormatDescription = deviceFormat.formatDescription
+            if CMFormatDescriptionGetMediaSubType(deviceFormatDescription) == kCVPixelFormatType_420YpCbCr8BiPlanarFullRange {
+                let candidateDimensions = CMVideoFormatDescriptionGetDimensions(deviceFormatDescription)
+                if (highestResolutionFormat == nil) || (candidateDimensions.width > highestResolutionDimensions.width) {
+                    highestResolutionFormat = deviceFormat
+                    highestResolutionDimensions = candidateDimensions
+                }
+            }
+        }
+        
+        if highestResolutionFormat != nil {
+            let resolution = CGSize(width: CGFloat(highestResolutionDimensions.width), height: CGFloat(highestResolutionDimensions.height))
+            return (highestResolutionFormat!, resolution)
+        }
+        
+        return nil
+    }
+    
+    fileprivate func teardownAVCapture() {
+        self.videoDataOutput = nil
+        self.videoDataOutputQueue = nil
+        
+        if let previewLayer = self.previewLayer {
+            previewLayer.removeFromSuperlayer()
+            self.previewLayer = nil
+        }
+    }
+    
+    //MARK:- Error presentation methods
+    fileprivate func presentErrorAlert(withTitle title: String = "Unexpected Failure", message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        self.present(ac, animated: true, completion: nil)
+    }
+    
+    fileprivate func presentError(_ error: NSError) {
+        self.presentErrorAlert(withTitle: "Failed with error \(error.code)", message: error.localizedDescription)
+    }
 
 }
 
